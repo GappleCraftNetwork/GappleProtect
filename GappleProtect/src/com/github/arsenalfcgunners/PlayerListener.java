3df68fff5580020e053e8fb41dbfc6d2c97e89ee
arsenalfcgunners <arsenalfcgunnersplaysmc@gmail.com>
2016-01-12 17:22:26 -0800

    Added PlayerInteractEvent

--- GappleProtect/src/com/github/arsenalfcgunners/PlayerListener.java
+++ GappleProtect/src/com/github/arsenalfcgunners/PlayerListener.java
@@ -1,10 +1,12 @@
 package com.github.arsenalfcgunners;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerAchievementAwardedEvent;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
@@ -12,6 +14,7 @@ import org.bukkit.event.player.PlayerBucketFillEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerExpChangeEvent;
 import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -91,6 +94,24 @@ public class PlayerListener implements Listener{
 	@EventHandler(priority = EventPriority.LOW)
 	public void onFish(PlayerFishEvent e){
 		e.setCancelled(true);
+	}
+	
+	@EventHandler(priority = EventPriority.LOW)
+	public void onInteract(PlayerInteractEvent e){
+		Player player = e.getPlayer();
+		if(e.getAction() == Action.PHYSICAL && e.getClickedBlock().getType() == Material.SOIL){
+            e.setCancelled(true);
+    	}
+		
+		if(!gp.getPlayerProfile(player).getBuildStatus()){      
+	        if(e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getClickedBlock().getType() == Material.TRAP_DOOR){
+	        	e.setCancelled(true);
+	    	}
+	        
+	        if(e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getClickedBlock().getType() == Material.FENCE_GATE){
+	        	e.setCancelled(true);
+	    	}
+		}
 	}
 	
 }

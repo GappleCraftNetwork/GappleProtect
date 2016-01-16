ac5b37b6ccd60c52ef6435e102a83f1cec0c3fe8
arsenalfcgunners <arsenalfcgunnersplaysmc@gmail.com>
2016-01-16 13:31:44 -0800

    Added fire spread and fire break block protection.

--- GappleProtect/src/com/github/arsenalfcgunners/gappleprotect/BlockListener.java
+++ GappleProtect/src/com/github/arsenalfcgunners/gappleprotect/BlockListener.java
@@ -1,10 +1,15 @@
 package com.github.arsenalfcgunners.gappleprotect;
 
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.block.BlockIgniteEvent;
+import org.bukkit.event.block.BlockIgniteEvent.IgniteCause;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 public class BlockListener implements Listener{
@@ -25,11 +30,31 @@ public class BlockListener implements Listener{
 	}
 	
 	@EventHandler(priority = EventPriority.LOWEST) 
-	public void onBlockBreak(BlockBreakEvent e) {
+	public void onBlockBreak(BlockBreakEvent e){
 		if(e.getBlock().getType() != Material.FIRE){
 	        if(!gp.getPlayerProfile(e.getPlayer()).getBuildStatus()){
 				e.setCancelled(true);
 	        }
 		}
 	}
+	
+	@EventHandler(priority = EventPriority.LOWEST) 
+    public void onBlockIgnite(BlockIgniteEvent e){
+        if (e.getCause() != IgniteCause.FLINT_AND_STEEL){
+            e.setCancelled(true);
+        }
+    }
+	
+	@EventHandler(priority = EventPriority.LOWEST) 
+    public void onBlockBurn(BlockBurnEvent e){
+		for (BlockFace face : BlockFace.values())
+        {
+            Block f = e.getBlock().getRelative(face);
+            if (f.getType() == Material.FIRE)
+            {
+                f.setType(Material.AIR);
+            }
+        }
+		e.setCancelled(true);
+    }
 }
